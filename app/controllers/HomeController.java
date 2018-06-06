c7d093154e413c38327f1296bd77a556266c94a3
yoshikawasusumu <yoshikawasusumu@192.168.23.156>
2018-06-06 16:11:57 +0900

    form

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,8 +1,11 @@
 package controllers;
 
+import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
+import javax.inject.Inject;
+import play.data.FormFactory;
 
 /**
  * This controller contains an action to handle HTTP requests
@@ -11,18 +14,38 @@ import views.html.index;
  */
 public class HomeController extends Controller {
 
-    /**
-     * An action that renders an HTML page with a welcome message.
-     * The configuration in the <code>routes</code> file means that
-     * this method will be called when the application receives a
-     * <code>GET</code> request with a path of <code>/</code>.
-     */
-    public Result index() {
+	//private Form<SampleForm> form;
+
+    //Form用の内部クラス
+	@Inject
+	FormFactory formFactory;
+
+	public static class SampleForm{
+		public String message;
+	}
+	// ルートにアクセスした際のAction
+	public Result index() {
+		return ok(index.render("何か書いて。", formFactory.form(SampleForm.class)));
+	}
+
+	// sendにアクセスした際のAction
+	public Result send() {
+		Form<SampleForm> f = formFactory.form(SampleForm.class).bindFromRequest();
+		if(!f.hasErrors()) {
+			SampleForm data = f.get();
+			String msg = "you typed : " + data.message;
+			return ok(index.render(msg,f));
+		} else {
+			return badRequest(index.render("Error", formFactory.form(SampleForm.class)));
+		}
+	}
+
+    /*public Result index() {
         return ok(index.render(
         		"これは最初の値です。",
         		"これは真ん中の引数です。",
         		"これは最後のテキストです。"
         		));
-    }
+    }*/
 
 }
